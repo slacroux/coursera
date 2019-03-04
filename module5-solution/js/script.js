acf9b30761f18f8881cfe58d58fbfe653f4e4254
Stefany Lacroux <slacroux@users.noreply.github.com>
2019-03-04 15:19:31 -0300

    Update script.js

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -83,8 +83,8 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
-  /*function (responseText) {*/
-  function(buildAndShowCategoriesHTML) {
+  function (responseText) {
+  /*function(buildAndShowCategoriesHTML) {
   /*function(buildAndShowHomeHTML) {*/
       document.querySelector("#main-content").innerHTML = responseText;
       /*document.querySelector("#main-content").innerHTML = buildAndShowCategoriesHTML;*/
@@ -103,19 +103,10 @@ function buildAndShowHomeHTML (categories) {
   $ajaxUtils.sendGetRequest(
     homeHtmlUrl,
     function (homeHtml) {
-      /*$ajaxUtils.sendGetRequest(
-        homeUrl,
-        function (homeUrl) {
-          switchMenuToActive();
-          var categoriesViewHtml = buildHomeViewHtml(categories, homeHtml, homeUrl);
-          insertHtml("#main-content", categoriesViewHtml);
-        },
-        false);*/
       // TODO: STEP 2: Here, call chooseRandomCategory, passing it retrieved 'categories'
       // Pay attention to what type of data that function returns vs what the chosenCategoryShortName
       // variable's name implies it expects.
       var chosenCategoryShortName = chooseRandomCategory(categories);
-
 
       // TODO: STEP 3: Substitute {{randomCategoryShortName}} in the home html snippet with the
       // chosen category from STEP 2. Use existing insertProperty function for that purpose.
