335615251af616d00b74651f77a59ca1a79e3aa1
Stefany Lacroux <slacroux@users.noreply.github.com>
2019-03-04 15:03:00 -0300

    Update script.js

--- module5-solution/js/script.js
+++ module5-solution/js/script.js
@@ -83,11 +83,11 @@ document.addEventListener("DOMContentLoaded", function (event) {
 showLoading("#main-content");
 $ajaxUtils.sendGetRequest(
   allCategoriesUrl,
-  /*function (responseText) {*/
+  function (responseText) {
  /* function(buildAndShowCategoriesHTML) {*/
-  function(buildAndShowHomeHTML) {
-      /*document.querySelector('#main-content').innerHTML = responseText;*/
-	document.querySelector('#main-content').innerHTML = buildAndShowHomeHTML;
+  /*function(buildAndShowHomeHTML) {*/
+      document.querySelector('#main-content').innerHTML = responseText;
+	/*document.querySelector('#main-content').innerHTML = buildAndShowHomeHTML;*/
   },
    // ***** <---- TODO: STEP 1: Substitute [...] ******
   true); // Explicitely setting the flag to get JSON from server processed into an object literal
